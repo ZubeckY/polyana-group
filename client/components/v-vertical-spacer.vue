37d2c68d5991c39d12e31f7a7560fca3676bba59
user <oleg_zub00@mail.ru>
2023-09-07 11:07:29 +0300

    Дизайнерские отели

---
+++ client/components/v-vertical-spacer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="vertical-spacer"></div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+@Component({})
+export default class VVerticalSpacer extends Vue {
+}
+</script>
+
