edd40762668265898b5bb85f1c4e2697f11dd54d
user <oleg_zub00@mail.ru>
2023-09-11 13:24:26 +0300

    footer dynamic photo

--- client/components/footer-comp.vue
+++ client/components/footer-comp.vue
@@ -8,41 +8,172 @@
 
           <div class="footer-seeYouAtHotel">
             <v-card-title class="footer-title">До встречи в наших отелях</v-card-title>
-            <div class="d-flex"></div>
+            <v-chip-group v-model="activeChip" mandatory column
+                          active-class="golden-gradient white--text">
+              <v-chip class="text-uppercase"
+                      v-for="(item, i) in localMapping"
+                      :key="item.id" :value="i"
+                      color="#FAFAFD"
+                      style="font-size: 14px; font-weight: 400; height: 38px;">
+                {{ item.title }}
+              </v-chip>
+            </v-chip-group>
 
             <v-row class="">
               <v-col cols="auto">
                 <v-card-title class="footer-title">Контакты</v-card-title>
-                <v-card-text></v-card-text>
-                <v-card-text></v-card-text>
-                <v-card-text></v-card-text>
+                <v-card-text style="white-space: pre-line"><span class="text-uppercase">Адрес:</span>
+                  Краснодарский край,
+                  Красная поляна,
+                  с. Эсто-Садок,
+                  Автомобильный
+                  переулок, 4
+                </v-card-text>
+                <v-card-text class="text-uppercase" style="white-space: pre-line"><span class="text-decoration-underline">8 938 555 5552</span>
+                  Отдел бронирования
+                  (09:00 - 21:00)
+                </v-card-text>
+                <v-card-text class="text-uppercase" style="white-space: pre-line"><span class="text-decoration-underline">8 938 555 5551</span>
+                  Рецепция (круглосуточно)
+                </v-card-text>
               </v-col>
-              <v-col></v-col>
+              <v-col>
+                <v-carousel v-model="activeSlide"
+                            style="width: 364px;border-radius: 50px"
+                            height="290px"
+                            hide-delimiters>
+                  <v-carousel-item style="border-radius: 50px"
+                                   v-for="(item, i) in localMapping[activeChip]['photos']"
+                                   :key="'photo-' + item.id">
+                    <footer-slide :item="item"/>
+                  </v-carousel-item>
+                </v-carousel>
+
+              </v-col>
             </v-row>
 
           </div>
 
           <div class="footer-helpInfo">
             <v-card-title class="footer-title">помощь и информация</v-card-title>
-            <v-card-subtitle>О Комплексе</v-card-subtitle>
+            <v-card-subtitle class="my-2 pb-0 text-uppercase" style="color: #000;font-size: 17px;font-weight: 700;">О Комплексе</v-card-subtitle>
 
-            О нас
-            Блог
-            Размещение
-            Услуги
-            Специальные предложения
-            Как добраться
-            Трансфер
-            Реквизиты
-            Контакты
+            <v-card-actions class="d-flex flex-column align-start pt-0 pl-2">
+              <v-btn class="text-none pl-2 pa-0 mb-1"
+                     width="auto"
+                     height="auto"
+                     min-width="0"
+                     min-height="0"
+                     color="black"
+                     text style="font-size: 16px">
+                О нас
+              </v-btn>
+              <v-btn class="text-none pa-0 mb-1"
+                     width="auto"
+                     height="auto"
+                     min-width="0"
+                     min-height="0"
+                     color="black"
+                     text style="font-size: 16px">
+                Блог
+              </v-btn>
+              <v-btn class="text-none pa-0 mb-1"
+                     width="auto"
+                     height="auto"
+                     min-width="0"
+                     min-height="0"
+                     color="black"
+                     text style="font-size: 16px">
+                Размещение
+              </v-btn>
+              <v-btn class="text-none pa-0 mb-1"
+                     width="auto"
+                     height="auto"
+                     min-width="0"
+                     min-height="0"
+                     color="black"
+                     text style="font-size: 16px">
+                Услуги
+              </v-btn>
+              <v-btn class="text-none pa-0 mb-1"
+                     width="auto"
+                     height="auto"
+                     min-width="0"
+                     min-height="0"
+                     color="black"
+                     text style="font-size: 16px">
+                Специальные предложения
+              </v-btn>
+              <v-btn class="text-none pa-0 mb-1"
+                     width="auto"
+                     height="auto"
+                     min-width="0"
+                     min-height="0"
+                     color="black"
+                     text style="font-size: 16px">
+                Как добраться
+              </v-btn>
+              <v-btn class="text-none pa-0 mb-1"
+                     width="auto"
+                     height="auto"
+                     min-width="0"
+                     min-height="0"
+                     color="black"
+                     text style="font-size: 16px">
+                Трансфер
+              </v-btn>
+              <v-btn class="text-none pa-0 mb-1"
+                     width="auto"
+                     height="auto"
+                     min-width="0"
+                     min-height="0"
+                     color="black"
+                     text style="font-size: 16px">
+                Реквизиты
+              </v-btn>
+              <v-btn class="text-none pa-0"
+                     width="auto"
+                     height="auto"
+                     min-width="0"
+                     min-height="0"
+                     color="black"
+                     text style="font-size: 16px">
+                Контакты
+              </v-btn>
+            </v-card-actions>
 
-            Частые вопросы
-            Инфраструктура
-            Активности на Красной поляне
-
-
-
+            <v-card-actions class="d-flex flex-column align-start pl-2">
+              <v-btn class="text-none pa-0 pl-2 mb-1"
+                     width="auto"
+                     height="auto"
+                     min-width="0"
+                     min-height="0"
+                     color="black"
+                     text style="font-size: 16px">
+                Частые вопросы
+              </v-btn>
+              <v-btn class="text-none pa-0 mb-1"
+                     width="auto"
+                     height="auto"
+                     min-width="0"
+                     min-height="0"
+                     color="black"
+                     text style="font-size: 16px">
+                Инфраструктура
+              </v-btn>
+              <v-btn class="text-none pa-0"
+                     width="auto"
+                     height="auto"
+                     min-width="0"
+                     min-height="0"
+                     color="black"
+                     text style="font-size: 16px">
+                Активности на Красной поляне
+              </v-btn>
+            </v-card-actions>
           </div>
+
+          <v-spacer/>
 
           <v-card class="footer-booking d-flex flex-column justify-center rounded-xxl"
                   width="318px" height="439px" color="#454850" elevation="0">
@@ -100,10 +231,68 @@
   </footer>
 </template>
 <script lang="ts">
-import {Vue, Component} from 'vue-property-decorator';
+import {Vue, Component, Watch} from 'vue-property-decorator';
 
 @Component({})
 export default class footerComp extends Vue {
+  activeChip: number = 0
+  activeSlide: number = 0
+  localMapping: any = [
+    {
+      id: 1,
+      priority: 1,
+      title: 'ULTIMA CLUB | HOTEL & SPA',
+      photos: [
+        {
+          id: 11,
+          priority: 1,
+          url: 'https://via.placeholder.com/1024x768/eee?text=4:3',
+        },
+        {
+          id: 12,
+          priority: 2,
+          url: 'https://via.placeholder.com/1024x768/eee?text=4:3',
+        },
+      ]
+    },
+    {
+      id: 2,
+      priority: 2,
+      title: 'IKOS POLYANA',
+      photos: [
+        {
+          id: 2,
+          priority: 1,
+          url: 'https://via.placeholder.com/1024x768/eee?text=4:3',
+        },
+      ]
+    },
+    {
+      id: 3,
+      priority: 3,
+      title: 'COUNTRY hills resort',
+      photos: [
+        {
+          id: 3,
+          priority: 1,
+          url: 'https://via.placeholder.com/1024x768/eee?text=4:3',
+        },
+      ]
+    },
+  ]
 
+  @Watch('activeChip')
+  changeController () {
+    this.changeMapping()
+    this.emptyActiveSlide ()
+  }
+
+  changeMapping () {
+    this.$emit('changeMapping', this.localMapping[this.activeChip]['htmltagyandexmap'])
+  }
+
+  emptyActiveSlide () {
+    return this.activeSlide = -1
+  }
 }
 </script>
---
+++ client/components/footer/slide.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card class="footer-slide"
+          elevation="0"
+          width="364px"
+          height="290px"
+          color="transparent"
+          style="border-radius: 50px">
+
+    <svg xmlns="http://www.w3.org/2000/svg"
+         width="364" height="290"
+         viewBox="0 0 364 290" fill="none">
+      <path d="M12.1333 0C5.43228 0 0 5.37259 0 12V278C0 284.627 5.43229 290 12.1333 290H271.222C283.372 290 293.222 280.15 293.222 268V242C293.222 229.85 303.181 220 315.467 220H342C354.15 220 364 210.15 364 198V12C364 5.37258 358.568 0 351.867 0H12.1333Z"
+            :fill="'url(#pattern'+item.id+')'"/>
+      <defs>
+        <pattern :id="'pattern'+item.id" patternContentUnits="objectBoundingBox" width="1" height="1">
+          <use :href="'#photo'+item.id" transform="matrix(0.000366166 0 0 0.000459602 0 -0.441264)"/>
+        </pattern>
+      </defs>
+
+      <image :id="'photo'+item.id"
+             width="2731" height="4096"
+             :href="item.url"/>
+    </svg>
+
+  </v-card>
+</template>
+<script lang="ts">
+import {Vue, Component, Prop} from 'vue-property-decorator'
+@Component({})
+export default class footerSlide extends Vue {
+  @Prop() item!: any
+
+
+
+  get logger () {
+    return console.log('ITEM')
+  }
+
+}
+</script>
