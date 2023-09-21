62b3b77a3511716a392a0e84f3fc7c4809ebe3d3
Oleg <oleg_zub00@mail.ru>
2023-09-21 11:30:51 +0300

    нукс конфиг ++

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 import colors from 'vuetify/es5/util/colors'
+const mode = 'production'
+const isDev = mode !== "production"
+
 export default {
-  server: { host: '0.0.0.0' },
   head: {
     titleTemplate: '%s - client',
     title: 'client',
@@ -8,12 +10,16 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'description', hid: 'description', content: 'Тестовое описание' },
+      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
   },
+
   components: true,
+
   css: [
     '~/assets/styles/screen/960px.less',
     '~/assets/styles/screen/1100px.less',
@@ -28,21 +34,29 @@ export default {
     '~/assets/styles/main.less',
     '~/assets/styles/ui-styles.less'
   ],
+
   plugins: [
     '~/plugins/v_mask.js'
   ],
+
   buildModules: [
-    '@nuxt/typescript-build',
     '@nuxtjs/vuetify',
+    'nuxt-webpack-optimisations',
+    '@nuxt/typescript-build',
     "nuxt-storm"
   ],
+
   modules: [
     '@nuxtjs/axios',
     '@nuxtjs/proxy',
     "cookie-universal-nuxt"
   ],
-  axios: {baseURL:'/', proxy:true},
+
+  axios: { baseURL:'/' },
+
   vuetify: {
+    optionsPath: './vuetify.options.js',
+    // treeShake: true,
     theme: {
       dark: false,
       themes: {
@@ -58,36 +72,63 @@ export default {
       }
     }
   },
+
+  webpackOptimisations: {
+    esbuildLoaderOptions: {
+      client: {
+        minifyIdentifiers: false,
+        target: 'es2015',
+      },
+      server: {
+        minifyIdentifiers: false,
+        target: 'node14',
+      },
+      modern: {
+        target: 'es2018',
+      },
+    }
+  },
+
   build: {
-    html:{
-      minify:{
-        minifyURLs: true,
-        minifyJS: true,
-        minifyCSS: true,
-        decodeEntities: true,
-        useShortDoctype: true,
-        collapseWhitespace: true,
-        trimCustomFragments: true,
-        preserveLineBreaks: false,
+    filenames: {
+      chunk: ({ isDev, isModern }) => isDev ? `[name]${isModern ? '.modern' : ''}.js` : `[contenthash:7]${isModern ? '.modern' : ''}.js`,
+      img: ({ isDev }) => isDev ? '[path][name].[ext]' : 'img/[name].[contenthash:7].[ext]',
+    },
+
+    html: {
+      minify: {
         collapseBooleanAttributes: true,
+        decodeEntities: true,
+        minifyCSS: true,
+        minifyJS: true,
         processConditionalComments: true,
-        removeRedundantAttributes: true,
         removeEmptyAttributes: true,
-        removeEmptyElements: true,
-        removeComments: true
+        removeRedundantAttributes: true,
+        trimCustomFragments: true,
+        useShortDoctype: true
       }
     },
-    transpile: ["swiper"],
+
+
+    // extractCSS: true,
+    // cssSourceMap: false,
+
     optimization: {
       minimize: true
     },
-    splitChunks: {
-      layouts: true,
-      pages: true,
-      commons: true,
-      cacheGroups: {
-        styles: {name: 'styles', test: /\.(css|less|vue)$/, chunks: 'all', enforce: true}
-      }
-    },
+    //   splitChunks: {
+    //     cacheGroups: {
+    //       styles: {
+    //         name: 'styles',
+    //         test: /\.(css|less|vue)$/,
+    //         chunks: 'all',
+    //         enforce: true
+    //       }
+    //     }
+    //   }
+    // },
+
+    transpile: ["swiper"]
+
   }
 }
