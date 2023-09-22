1ed0a70ce23797e52f7cdce405d58c4d440acff0
user <oleg_zub00@mail.ru>
2023-09-22 13:11:53 +0300

    footer-components

--- components/footer/comp.vue
+++ components/footer/comp.vue
@@ -59,15 +59,8 @@
               <v-card-title class="footer-helpInfo-title footer-title">помощь и информация</v-card-title>
               <v-card-subtitle class="footer-helpInfo-subtitle my-2 pb-0 text-uppercase">О Комплексе</v-card-subtitle>
 
-              <v-card-actions class="pa-0 ml-4">
-                <div class="d-flex flex-column align-start">
-                  <v-btn v-for="(item, i) in links"
-                         color="black" v-html="item['title']" text
-                         :key="item.link+'-'+i" :disabled="item.disabled"
-                         class="footer-helpInfo-link text-none pa-0 mb-1"
-                         width="auto" height="auto" min-width="0" min-height="0"/>
-                </div>
-              </v-card-actions>
+              <footer-links/>
+
             </div>
             <v-spacer/>
             <!-- Забронировать -->
@@ -140,22 +133,6 @@ export default class FooterComp extends Vue {
         },
       ]
     },
-  ]
-
-  links: any = [
-    { link: '/', title: 'О нас', },
-    { link: '/', title: 'Блог', },
-    { link: '/', title: 'Размещение', },
-    { link: '/', title: 'Услуги', },
-    { link: '/', title: 'Специальные предложения', },
-    { link: '/', title: 'Как добраться', },
-    { link: '/', title: 'Трансфер', },
-    { link: '/', title: 'Реквизиты', },
-    { link: '/', title: 'Контакты', },
-    { link: '',  title: '&nbsp;', disabled: true },
-    { link: '/', title: 'Частые вопросы', },
-    { link: '/', title: 'Инфраструктура', },
-    { link: '/', title: 'Активности на Красной поляне', },
   ]
 
   @Watch('activeChip')
---
+++ components/footer/links.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card-actions class="pa-0 ml-4">
+    <div class="d-flex flex-column align-start">
+      <v-btn v-for="(item, i) in links"
+             color="black" text
+             v-html="item['title']"
+             :key="itemLink(item, i)"
+             :disabled="itemDisabled(item)"
+             class="footer-helpInfo-link text-none pa-0 mb-1"
+             width="auto" height="auto"
+             min-width="0" min-height="0"/>
+    </div>
+  </v-card-actions>
+</template>
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+@Component({})
+export default class links extends Vue {
+  links: any = [
+    { link: '/', title: 'О нас', },
+    { link: '/', title: 'Блог', },
+    { link: '/', title: 'Размещение', },
+    { link: '/', title: 'Услуги', },
+    { link: '/', title: 'Специальные предложения', },
+    { link: '/', title: 'Как добраться', },
+    { link: '/', title: 'Трансфер', },
+    { link: '/', title: 'Реквизиты', },
+    { link: '/', title: 'Контакты', },
+    { link: '',  title: '&nbsp;', disabled: true },
+    { link: '/', title: 'Частые вопросы', },
+    { link: '/', title: 'Инфраструктура', },
+    { link: '/', title: 'Активности на Красной поляне', },
+  ]
+  itemLink (item: any, i: any) {return item.link+'-'+i}
+  itemDisabled (item: any) {return item.disabled}
+}
+</script>
