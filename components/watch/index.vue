10b6463d567b7097f9e082e75ab5c39fda9ae584
user <oleg_zub00@mail.ru>
2023-09-28 17:41:40 +0300

    Диз. отели_1

--- components/watch/index.vue
+++ components/watch/index.vue
@@ -2,13 +2,13 @@
   <div class="watch-video">
     <div class="watch-video-container">
       <watch-rotate/>
-      <watch-play/>
+      <watch-arrow/>
     </div>
   </div>
 </template>
 <script lang="ts">
 import {Vue, Component} from 'vue-property-decorator';
 @Component({})
-export default class index extends Vue {
+export default class Watch extends Vue {
 }
 </script>
