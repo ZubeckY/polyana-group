7bc536428ee1b6c94920245913ca45062bc9b1cf
user <oleg_zub00@mail.ru>
2023-10-03 15:16:54 +0300

    adaptive-1-page

--- components/reviews/index.vue
+++ components/reviews/index.vue
@@ -11,20 +11,21 @@
         <!-- -->
         <div class="reviews-cards-group d-flex mt-9">
           <div class="reviews-cards-group-gradient">
-            <v-card class="reviews-cards-group-gradient-card d-flex align-center rounded-xl px-4"
+            <v-card class="reviews-cards-group-gradient-card d-flex align-center rounded-xl"
                     elevation="0" color="golden-gradient">
 
               <v-card class="reviews-cards-group-gradient-card-round d-flex justify-center align-center mr-3"
                       rounded="circle" elevation="0">
                 <v-img class="reviews-cards-group-gradient-card-round-img d-block" alt="Я"
-                       contain src="img/reviews/yandex.webp" />
+                       contain src="img/reviews/yandex.webp"/>
               </v-card>
-              <v-card-title class="reviews-cards-group-gradient-title text-uppercase pa-0">Смотреть отзывы
-                в Яндекс</v-card-title>
+              <div class="reviews-cards-group-gradient-title text-uppercase pa-0">Смотреть отзывы
+                в Яндекс
+              </div>
 
             </v-card>
 
-            <v-card class="reviews-cards-group-gradient-card d-flex align-center rounded-xl mt-4 px-4"
+            <v-card class="reviews-cards-group-gradient-card d-flex align-center rounded-xl mt-4"
                     elevation="0" color="golden-gradient">
 
               <v-card class="reviews-cards-group-gradient-card-round d-flex justify-center align-center mr-3"
@@ -32,14 +33,15 @@
                 <v-img class="reviews-cards-group-gradient-card-round-img d-block" alt="TL"
                        contain src="img/reviews/tl-rep.webp"/>
               </v-card>
-              <v-card-title class="reviews-cards-group-gradient-title text-uppercase pa-0">Смотреть отзывы
-                в TL: REPUTATION</v-card-title>
+              <div class="reviews-cards-group-gradient-title text-uppercase pa-0">Смотреть отзывы
+                в TL: REPUTATION
+              </div>
 
             </v-card>
           </div>
 
-          <div class="reviews-cards-group-transparent ml-8">
-            <v-card elevation="0" height="78px" color="transparent">
+          <div class="reviews-cards-group-transparent">
+            <div>
               <div class="reviews-cards-group-transparent-title pa-0"><b>5,0</b> рейтинг в Яндекс
                 на основе 1300+ отзывов
               </div>
@@ -52,15 +54,16 @@
                 <!--                  <span></span>-->
                 <!--                </div>-->
               </v-btn>
-            </v-card>
+            </div>
 
-            <v-card class="mt-4" elevation="0" height="78px" color="transparent">
+            <div class="mt-4">
               <div class="reviews-cards-group-transparent-title pa-0"><b>9,6</b> рейтинг в TravelLine
                 на основе 800+ отзывов
               </div>
               <v-btn class="reviews-cards-group-transparent-button pa-0 mt-1"
-                     color="#CCAC6C" height="auto" in-width="0" min-height="0" text>ОСТАВИТЬ ОТЗЫВ</v-btn>
-            </v-card>
+                     color="#CCAC6C" height="auto" in-width="0" min-height="0" text>ОСТАВИТЬ ОТЗЫВ
+              </v-btn>
+            </div>
           </div>
         </div>
       </div>
@@ -76,12 +79,10 @@
           <v-btn elevation="0" rounded width="40px" height="40px" min-height="0" min-width="0" title="Вперёд">
             <chevron-right/>
           </v-btn>
-
         </v-card-actions>
 
         <div class="">
-          <v-card class="reviews-slider-card rounded-xxl" elevation="0" width="558px" height="289px">
-          </v-card>
+          <v-card class="reviews-slider-card rounded-xxl" elevation="0" width="558px" height="289px"></v-card>
         </div>
       </div>
 
@@ -90,6 +91,7 @@
 </template>
 <script lang="ts">
 import {Vue, Component} from 'vue-property-decorator';
+
 @Component({})
 export default class Reviews extends Vue {
 }
