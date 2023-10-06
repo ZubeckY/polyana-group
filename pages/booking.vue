24ec749b9be58b36b453b42c2e54a3ba5c845e05
user <oleg_zub00@mail.ru>
2023-10-06 12:05:42 +0300

    booking

---
+++ pages/booking.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <header class="header header-inside">
+      <div class="header-inside-container">
+        <div class="header-inside-container-inner">
+          <header-nav-ultima/>
+        </div>
+      </div>
+    </header>
+
+    <section class="tl-section">
+      <div class="tl-section-container">
+        <!-- start TL Booking form script -->
+        <div id="tl-booking-form">&nbsp;</div>
+        <!-- end TL Booking form script -->
+      </div>
+    </section>
+  </div>
+</template>
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component({})
+export default class Booking extends Vue {
+}
+</script>
