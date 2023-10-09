334afb12d3355066b11124f7dc746842e8c9bbed
oleg <oleg_zub00@mail.ru>
2023-10-09 18:50:34 +0300

    правуки

---
+++ components/watch/inside.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="watch-video inside">
+    <div class="watch-video-container">
+      <watch-rotate-inside/>
+      <watch-arrow/>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+@Component({})
+export default class Watch extends Vue {
+}
+</script>
