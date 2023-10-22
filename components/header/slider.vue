2e4f875f89798411576c3dbccf1545c6967187ce
user <oleg_zub00@mail.ru>
2023-10-22 13:31:50 +0300

    предзагрузка

--- components/header/slider.vue
+++ components/header/slider.vue
@@ -9,8 +9,8 @@
       </v-btn>
 
       <v-btn-toggle class="header-slider__slider" v-model="slide"
-                    color="rgba(255, 255, 255, 0.75)" mandatory>
-        <v-btn v-for="(item, i) in slider" :key="'header-slide' + i"
+                    color="rgba(255, 255, 255, 0.85)" mandatory>
+        <v-btn v-for="(item, i) in small" :key="'header-slide' + i"
                class="header-slider__slide d-flex justify-center align-center"
                title="Слайд 1" color="rgba(255, 255, 255, 0.32)" fab :value="i">
           <nuxt-img width="51px" height="51px" style="border-radius: 100px" loading="lazy"
@@ -37,6 +37,12 @@ export default class Slider extends Vue {
     "https://thumb.cloud.mail.ru/weblink/thumb/xw1/DWvM/gXMjkb9TC/Country%20Hills%20Resort%202/1%20%D0%AD%D0%BA%D1%81%D1%82%D0%B5%D1%80%D1%8C%D0%B5%D1%80%20%2B%20Ikos/_MG_2455.jpg",
     "https://ztgxmhicyraofyrgiitp.supabase.co/storage/v1/object/public/publicimg/hotels/Country%20Hills/main/main1.webp",
     "https://thumb.cloud.mail.ru/weblink/thumb/xw1/DWvM/gXMjkb9TC/%D0%9E%D1%82%D0%B5%D0%BB%D1%8C%20Ultima%20Club%202/%D1%8D%D0%BA%D1%81%D1%82%D0%B5%D1%80%D1%8C%D0%B5%D1%80/_MG_7748.jpg"
+  ]
+
+  small: any = [
+    'https://ztgxmhicyraofyrgiitp.supabase.co/storage/v1/object/public/publicimg/mainbanner/small/Frame-16.webp',
+    "https://ztgxmhicyraofyrgiitp.supabase.co/storage/v1/object/public/publicimg/hotels/Country%20Hills/main/main1.webp",
+    'https://ztgxmhicyraofyrgiitp.supabase.co/storage/v1/object/public/publicimg/mainbanner/small/Frame-13.webp'
   ]
 
   @Watch('slide')
