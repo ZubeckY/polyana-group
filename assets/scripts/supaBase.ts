a6e773b0c6f7b82853e96bdbd74264d1e5ba57df
oleg <oleg_zub00@mail.ru>
2023-12-07 17:58:29 +0300

    fix supabase

--- assets/scripts/supaBase.ts
+++ assets/scripts/supaBase.ts
@@ -1,15 +1,28 @@
 require('dotenv').config()
-import { createClient } from '@supabase/supabase-js'
+import {createClient} from '@supabase/supabase-js'
 
-const devMode = Boolean(process.env.DEV_MODE)
-
-const baseUrl:any = process.env.BASE_URL
+const devMode = process.env.DEV_MODE
+const baseUrl: any = process.env.BASE_URL
 const apiPrefix: any = process.env.SUPA_BASE_API_PREFIX
-const supaBaseUrlEnv = process.env.SUPA_BASE_URL
 
-const supaBaseUrl: any =  devMode ? supaBaseUrlEnv : baseUrl + apiPrefix
+const supaBaseUrDev = process.env.SUPA_BASE_URL
 const supaBaseKey = process.env.SUPA_BASE_KEY
+const supaBaseUrl: any = returnBoolean() ? supaBaseUrDev : baseUrl + apiPrefix
+
 //@ts-ignore
 const supaBase: any = createClient(supaBaseUrl, supaBaseKey)
-
 export default supaBase
+
+function returnBoolean() {
+  const isDevMode = String(devMode).toLowerCase()
+  switch (isDevMode) {
+    case '0':
+    case 'false':
+      return false
+    case '1':
+    case 'true':
+      return true
+    default:
+      return true
+  }
+}
