4a3fc1fb6614a1ee8166ca1e16522abf03edfe05
oleg <oleg_zub00@mail.ru>
2023-12-12 13:45:46 +0300

    test

--- assets/scripts/supaBase.ts
+++ assets/scripts/supaBase.ts
@@ -1,7 +1,7 @@
 require('dotenv').config()
 import {createClient} from '@supabase/supabase-js'
 
-const devMode = process.env.DEV_MODE
+const DEV_MODE = process.env.SUPA_BASE_DEV_MODE
 const baseUrl: any = process.env.BASE_URL
 const apiPrefix: any = process.env.SUPA_BASE_API_PREFIX
 
@@ -9,12 +9,8 @@ const supaBaseUrDev = process.env.SUPA_BASE_URL
 const supaBaseKey = process.env.SUPA_BASE_KEY
 const supaBaseUrl: any = returnBoolean() ? supaBaseUrDev : baseUrl + apiPrefix
 
-//@ts-ignore
-const supaBase: any = createClient(supaBaseUrl, supaBaseKey)
-export default supaBase
-
-function returnBoolean() {
-  const isDevMode = String(devMode).toLowerCase()
+function returnBoolean () {
+  const isDevMode = String(DEV_MODE).toLowerCase()
   switch (isDevMode) {
     case '0':
     case 'false':
@@ -26,3 +22,41 @@ function returnBoolean() {
       return true
   }
 }
+
+// @ts-ignore
+const supaBase: any = createClient(supaBaseUrl, supaBaseKey)
+export default supaBase
+
+
+// require('dotenv').config()
+// import {createClient} from '@supabase/supabase-js'
+//
+// const DEV_MODE = process.env.SUPA_BASE_DEV_MODE
+// const BASE_URL = process.env.BASE_URL
+//
+// const SUPA_BASE_KEY_DEV = process.env.SUPA_BASE_KEY_DEV
+// const SUPA_BASE_KEY_NORMAL = process.env.SUPA_BASE_KEY
+//
+// const SUPA_BASE_URL_DEV = process.env.SUPA_BASE_URL_DEV
+// const SUPA_BASE_URL_NORMAL = process.env.SUPA_BASE_URL
+//
+// const SUPA_BASE_URL = devModeNormalize() ? BASE_URL + 'api/v1/data-base/' : SUPA_BASE_URL_NORMAL
+// const SUPA_BASE_KEY = devModeNormalize() ? SUPA_BASE_KEY_DEV : SUPA_BASE_KEY_NORMAL
+//
+// function devModeNormalize () {
+//   const isDevMode = String(DEV_MODE).toLowerCase()
+//   switch (isDevMode) {
+//     case '0':
+//     case 'false':
+//       return false
+//     case '1':
+//     case 'true':
+//       return true
+//     default:
+//       return true
+//   }
+// }
+//
+// //@ts-ignore
+// const supaBase: any = createClient(SUPA_BASE_URL, SUPA_BASE_KEY)
+// export default supaBase
